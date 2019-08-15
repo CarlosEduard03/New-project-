9d3a15ca8b52119135069d64606482b760959c64
Carlosetec <carloseduardoetecab@gmail.com>
2019-08-16 01:38:07 +0900

    Tentando terminar o JavaScript

--- www/components/a.js
+++ www/components/a.js
@@ -1,8 +1,37 @@
 
-$(document).on("click","#apagar",function(){
-v1= parseFloat($(resultado).val());
-$("#idade").val("");
-$("#altura").val("");
-$("#peso").val("");
-var re = $("#resultado").val();
+$(document).on("click","#calcular",function(){
+
+  var feminino;
+  var masculino;
+  var peso;
+  var altura;
+  var idade;
+  var nivel;
+  var resultado;
+  var sexo;
+  
+  
+ 
+ 
+
+   $idade = $("#idade").val();
+   $peso = $("#peso").val();
+   $altura = $("#altura").val();
+   $feminino = $("input[id = 'mulher']:checked").val();
+   $masculino = $("input[id = 'homem']:checked").val();
+   $nivel = $("#nivelAtividade").val();
+   $sexo = $("#sexo").val();
+   $man = $("masculino").val();
+   $woman = $("feminino").val();
+
+  if($sexo == 'masculino'){
+
+    $resultado = $nivel * (66 + ((13.7 *  $peso) + (5 * $altura) - (6.8 * $idade)));
+    $("#visor").val($resultado);
+
+  }
+  if($sexo == 'feminino'){
+    $resultado = $nivel *(655 + ((9.6 * $peso) + (1.8 * $altura) - (4.7 * $idade)) + 0.5);
+       $("#visor").val($resultado);
+  }
 });
